02d946b6c40e5cb1fa66ed8252da12a996ce32f1
李孟 <limeng@knowlegene.com>
2020-07-09 17:56:19 +0800

    项目版本

--- client/kd-process/src/main/java/com/knowlegene/parent/process/runners/options/SwapFlinkPipelineOptions.java
+++ client/kd-process/src/main/java/com/knowlegene/parent/process/runners/options/SwapFlinkPipelineOptions.java
@@ -1,6 +1,6 @@
 package com.knowlegene.parent.process.runners.options;
 
-import org.apache.beam.runners.spark.SparkPipelineOptions;
+import org.apache.beam.runners.flink.FlinkPipelineOptions;
 
 /**
  * @Classname SwapFlinkPipelineOptions
@@ -8,5 +8,5 @@ import org.apache.beam.runners.spark.SparkPipelineOptions;
  * @Date 2020/7/9 17:25
  * @Created by limeng
  */
-public interface SwapFlinkPipelineOptions  extends SparkPipelineOptions,BasePipelineOptions,BaseFunPipelineOptions {
+public interface SwapFlinkPipelineOptions  extends FlinkPipelineOptions,BasePipelineOptions,BaseFunPipelineOptions {
 }
